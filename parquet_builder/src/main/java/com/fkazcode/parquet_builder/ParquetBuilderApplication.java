3c5760af90d46aa2d428c979a6b27e32caba57ba
Felipe P. Cavalcanti <98607675+fpcavalcanti@users.noreply.github.com>
2023-03-06 03:41:22 -0300

    added Spring boot project

---
+++ parquet_builder/src/main/java/com/fkazcode/parquet_builder/ParquetBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.fkazcode.parquet_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParquetBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParquetBuilderApplication.class, args);
+	}
+
+}
---
+++ parquet_builder/src/test/java/com/fkazcode/parquet_builder/ParquetBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fkazcode.parquet_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParquetBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
